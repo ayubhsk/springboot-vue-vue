aa7264cb133206923d567cf5081bd411fb38acf1
w hx <1606175484@qq.com>
2022-03-27 03:09:11 +0800

    用户页面增删改查完成

--- src/main.js
+++ src/main.js
@@ -4,9 +4,26 @@ import router from './router'
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/assets/gloable.css'
+import request from "@/utils/request";
+import axios from "axios";
+
 Vue.config.productionTip = false
 
-Vue.use(ElementUI,{size:"small"});
+Vue.use(ElementUI, { size: "mini" });
+
+Vue.prototype.request=request
+
+axios.defaults.baseURL = 'http://localhost:8895'
+Vue.prototype.$axios=axios
+
+axios.interceptors.request.use(config => {
+  console.log(config);
+  // 请求头挂载信息
+  config.headers.Authorization = window.sessionStorage.getItem("flag");
+  // 在最后必须return config
+  return config;
+})
+
 
 new Vue({
   router,
